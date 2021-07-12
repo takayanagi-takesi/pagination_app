f611fa0c00fa3c8aaf535eefd16a92230c68379c
takayanagi-takesi <75218115+takayanagi-takesi@users.noreply.github.com>
2021-07-13 01:53:14 +0900

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,22 @@ class PostsController < ApplicationController
 
   PER_PAGE = 10
 
+  def new
+    @post = Post.new
+  end
+
   def index
     @posts = Post.page(params[:page]).per(PER_PAGE)
   end
 
   def create
-    
+    post = Post.create!(post_params)
+    redirect_to post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
